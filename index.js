262f6076d89f8b685a9b6146c606ed52488b02e4
Hugh Morrison <hmorri32@gmail.com>
2018-05-09 20:59:35 -0600

    ?

--- index.js
+++ index.js
@@ -1,145 +1,125 @@
-var guessInput   = document.getElementById("guess");
-var submitBTN    = document.getElementById("submit");
-var clearBTN     = document.getElementById("clear-input");
+var guessInput = document.getElementById("guess");
+var submitBTN = document.getElementById("submit");
+var clearBTN = document.getElementById("clear-input");
 var lastGuessWas = document.getElementById("last-guess-was");
 var lastGuessNum = document.getElementById("last-guess-num");
-var outcomeText  = document.getElementById("outcome-text");
-var resetBTN     = document.getElementById("reset");
-var newMin       = document.getElementById("new-min");
-var newMax       = document.getElementById("new-max");
-var resetNew     = document.getElementById("reset-new");
-var min          = 0;
-var max          = 100;
-var randomNumber;
-window.onload    = randomNumber = Math.floor(Math.random() * 100 + 1);
+var outcomeText = document.getElementById("outcome-text");
+var resetBTN = document.getElementById("reset");
+var newMin = document.getElementById("new-min");
+var newMax = document.getElementById("new-max");
+var resetNew = document.getElementById("reset-new");
+var min = 0;
+var max = 100;
+var randomNumber = Math.floor(Math.random() * 100 + 1);
+var userGuess;
 console.log(randomNumber);
 
-var userGuess = parseInt(guessInput.value);
+(() => {
+  disableButtons();
+})();
 
-resetNew.disabled  = true;
-resetBTN.disabled  = true;
-clearBTN.disabled  = true;
-submitBTN.disabled = true;
-
-// Enables clear button after user inputs guess
-
-guessInput.addEventListener("keyup", function() {
-  clearBTN.disabled  = false;
+const enableSubmitBtns = () => {
+  clearBTN.disabled = false;
   submitBTN.disabled = false;
-});
+};
 
-submitBTN.addEventListener("click", function() {
-  alertHelper();
+const submit = () => {
   userGuessHelper();
-  var userGuess          = parseInt(guessInput.value);
-  resetBTN.disabled      = false;
+  updateDom();
+};
+
+const updateDom = params => {
+  var userGuess = parseInt(guessInput.value);
+  resetBTN.disabled = false;
   lastGuessNum.innerText = userGuess;
   lastGuessWas.innerText = "Your last guess was:";
-});
+};
 
-// Helper Functions for Submit Button
-function alertHelper() {
-  if (guessInput.value === "") {
-    alert("Input an actual number pls.");
-  } else if (userGuess < 1 || userGuess > 100) {
-    alert("Input a number between 1 and 100 pls.");
-  }
-}
+guessInput.addEventListener("keyup", enableSubmitBtns);
+submitBTN.addEventListener("click", submit);
+clearBTN.addEventListener("click", clearStuff);
+resetBTN.addEventListener("click", resetGame);
 
-function userGuessHelper() {
+const userGuessHelper = () => {
   var userGuess = parseInt(guessInput.value);
-
   if (userGuess === randomNumber) {
     outcomeText.innerText = "... and that's exactly what I had in mind!";
-    alert("dassit!");
   } else if (userGuess < randomNumber) {
     outcomeText.innerText = "... and it was too low. try again.";
   } else {
     outcomeText.innerText = "... and it was too high. Try again.";
   }
-}
+};
 
-// disable clearBTN
-clearBTN.addEventListener("click", function() {
+const clearStuff = () => {
   guessInput.value = "";
-  disableClearBTN();
-});
+  clearBTN.disabled = true;
+};
 
-// Function helper that clears input
-function disableClearBTN() {
-  if (guessInput.value === "") {
-    clearBTN.disabled = true;
-  }
-}
-
-// reset everything
-resetBTN.addEventListener("click", function() {
+const resetGame = () => {
   randomNumber = Math.floor(Math.random() * 100 + 1);
   disableButtons();
   resetInputFieldValue();
   resetGuessText();
-});
+};
 
-// reset helper functions
-function resetGuessText() {
-  outcomeText.innerText  = "";
+const resetGuessText = () => {
+  outcomeText.innerText = "";
   lastGuessWas.innerText = "";
   lastGuessNum.innerText = "";
-}
+};
 
-function resetInputFieldValue() {
+const resetInputFieldValue = () => {
   guessInput.value = "";
-  newMax.value     = "";
-  newMin.value     = "";
-}
+  newMax.value = "";
+  newMin.value = "";
+};
 
-function disableButtons() {
-  resetBTN.disabled  = true;
-  clearBTN.disabled  = true;
-  resetNew.disabled  = true;
+const disableButtons = () => {
+  resetBTN.disabled = true;
+  clearBTN.disabled = true;
+  resetNew.disabled = true;
   submitBTN.disabled = true;
-}
+};
 
-// CHANGE RANGE
-function changeRange(min, max) {
+const changeRange = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
-newMin.addEventListener("keyup", function() {
+newMin.addEventListener("keyup", () => {
   min = parseInt(newMin.value);
   randomNumber = changeRange(min, max);
 });
 
-newMax.addEventListener("keyup", function() {
+newMax.addEventListener("keyup", () => {
   max = parseInt(newMax.value);
   randomNumber = changeRange(min, max);
 });
 
-// reset new parameters
-resetNew.addEventListener("click", function() {
+resetNew.addEventListener("click", () => {
   randomNumber = Math.floor(Math.random() * 100 + 1);
   clearOwnRangeSection();
   disableClearNewBtn();
 });
 
-function clearOwnRangeSection() {
+const clearOwnRangeSection = () => {
   newMax.value = "";
   newMin.value = "";
 }
 
-function disableClearNewBtn() {
+const disableClearNewBtn = () => {
   resetNew.disabled = true;
 }
 
 // Input field keyup to enable Clear New Range btn
-newMin.addEventListener("keyup", function() {
+newMin.addEventListener("keyup", () => {
   resetNewRangeButton();
 });
 
-newMax.addEventListener("keyup", function() {
+newMax.addEventListener("keyup", () => {
   resetNewRangeButton();
 });
 // User range button helper
-function resetNewRangeButton() {
+const resetNewRangeButton = () => {
   resetNew.disabled = false;
 }
